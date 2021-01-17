674bfff575a063556fcebd881d00d0ed5cd317cf
zecarloz <josecarlos2503@hotmail.com>
2021-01-17 18:39:42 -0300

    Criação projeto spring myapp

---
+++ src/main/java/com/zecarloz/myapi/MyappApplication.java
@@ -0,0 +1,13 @@
+package com.zecarloz.myapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zecarloz/myapi/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zecarloz.myapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
